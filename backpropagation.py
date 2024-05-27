07b7a67c40bc32d9dd4281e85e4551fbe145bd62
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2024-05-27 15:01:08 +0200

    backpropagation

--- backpropagation.py
+++ backpropagation.py
@@ -1,9 +1,23 @@
 import torch 
 import numpy as np
 
+#backpropagation calculating loss 
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print("CUDA is being used")
+    x = torch.tensor(1.0)
+    y = torch.tensor(2.0)
+
+    w = torch.tensor(1.0, requires_grad=True)
+
+    #forward pass to compute the loss
+    y_hat = w*x
+    loss = (y_hat - y)**2
+    print(loss)
+
+    #backward pass
+    loss.backward()
+    print(w.grad)
 
 
 else:
---
+++ gradient_descent_with_AutoGrad&backpropagation.py
@@ -0,0 +1,13 @@
+import torch 
+import numpy as np
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("CUDA is being used")
+ 
+
+else:
+    print("CUDA is not available")
+
+
