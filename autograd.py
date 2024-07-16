c1acf5c981f3dda21cd5773e0628a9d90edc6098
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-07-16 15:29:11 +0900

    modyfing code to run on new macbook

--- autograd.py
+++ autograd.py
@@ -1,59 +1,47 @@
-import torch 
-import numpy as np 
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-#gradients track the computational history. this gives us the ability to map out the progression of our data 
-#whenver we want to calculate gradients we need the grad requires to be true 
-#we can then calculate the gradients by calling the backward function
-#before we want to create the next iteration of our training loop we must call the zero function 
-#this will create a computatiopnal graph for gradient calculation
-    x = torch.rand(3, requires_grad=True, device=device)
-    print(x)
-    y=x+2
+import torch
+import numpy as np
+
+# Initialize tensors without checking for CUDA
+x = torch.rand(3, requires_grad=True)
+print(x)
+y = x + 2
+print(y)
+z = y * y * 2
+print(z)
+# z = z.mean()
+print(z)
+
+v = torch.tensor([0.1, 1.0, 0.001], dtype=torch.float32)  # Calculating the backward can only be done with scalar values or in other words
+                                                         # values that have calculations
+
+z.backward(v)  # Calculate the gradient of z with respect to x
+# print(x.grad)
+
+# Remove the grad requirement
+x.requires_grad_(False)
+print(x)
+
+# Another way is using detach that will create a new tensor from the same values of the tensor x but without the grad requirement
+y = x.detach()
+print(y)
+
+# This will wrap it and again prevent the grad function
+with torch.no_grad():
+    y = x + 2
     print(y)
-    z=y*y*2
-    print(z)
-    # z=z.mean()
-    print(z)
 
-    v = torch.tensor([0.1, 1.0, 0.001], dtype=torch.float32, device=device) # claculating the backward can only be done with scalar values or in other words
-                                                            # values that have calculations
+# Example of a training loop where grad is used 
+# Here we create tensor of 4 which requires grad
+weights = torch.ones(4, requires_grad=True)
 
-    z.backward(v) # calculate the gradient of z to the gradient of x
-    # print(x.grad)
-
-
-    #removes the grad requirment
-    x.requires_grad_(False)
-    print(x)
-
-    #another way is using detach that will create a new tensor from the same values of the tensor x but without the grad requirment
-    y = x.detach()
-    print(y)
-
-    #this will wrap it and again prevent the grad function
-    with torch.no_grad():
-        y = x + 2
-        print(y)
-
-
-
-    #example of a training loop where grad is used 
-    #here we create tensor of 4 which requires grad
-    weights = torch.ones(4, requires_grad=True, device=device)
-
-    #this is the training loop
-    # value in range is the number of iterations I want
-    for epoch in range(5):
-        #simple computation to show the issues that can come when using grad
-        model_output = (weights*3).sum()
-        #this applies the grad backward function whick will add the values from each iteration on to itself
-        model_output.backward()
-        print(weights.grad)
-        #to prevent that we call this below
-        #this usually done with pytorchs inbuilt optimizers
-        weights.grad.zero_()
-else:
-    print("CUDA is not available")
-
-
+# This is the training loop
+# Value in range is the number of iterations I want
+for epoch in range(5):
+    # Simple computation to show the issues that can come when using grad
+    model_output = (weights * 3).sum()
+    # This applies the grad backward function which will add the values from each iteration on to itself
+    model_output.backward()
+    print(weights.grad)
+    # To prevent that we call this below
+    # This is usually done with PyTorch's inbuilt optimizers
+    weights.grad.zero_()
--- logicalregression.py
+++ logicalregression.py
@@ -5,9 +5,15 @@ from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+# Check if MPS is available and set the device
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
 # 0) preparing data
 
-# breast cancer dataset
+# Breast cancer dataset
 bc = datasets.load_breast_cancer()
 x, y = bc.data, bc.target
 
@@ -15,15 +21,15 @@ n_samples, n_features = x.shape
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
-# scale
+# Scale
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
-x_train = torch.from_numpy(x_train.astype(np.float32))
-x_test = torch.from_numpy(x_test.astype(np.float32))
-y_train = torch.from_numpy(y_train.astype(np.float32))
-y_test = torch.from_numpy(y_test.astype(np.float32))
+x_train = torch.from_numpy(x_train.astype(np.float32)).to(device)
+x_test = torch.from_numpy(x_test.astype(np.float32)).to(device)
+y_train = torch.from_numpy(y_train.astype(np.float32)).to(device)
+y_test = torch.from_numpy(y_test.astype(np.float32)).to(device)
 
 y_train = y_train.view(y_train.shape[0], 1)
 y_test = y_test.view(y_test.shape[0], 1)
@@ -34,15 +40,15 @@ class LogisticRegression(nn.Module):
         super(LogisticRegression, self).__init__()
         self.linear = nn.Linear(n_input_features, 1)
 
-    # f= wx +b, sigmoid at the end
+    # f = wx + b, sigmoid at the end
     def forward(self, x):
         y_predicted = torch.sigmoid(self.linear(x))
         return y_predicted
 
-model = LogisticRegression(n_features)
+model = LogisticRegression(n_features).to(device)
 
 # 2) loss and optimizer
-learning_rate = 0.4
+learning_rate = 0.01
 criterion = nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
